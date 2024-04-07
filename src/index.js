ea9b5ecbcfc3c0fe100c6f007157b1813ac0701e
YAZZYD <islam.yazid.ben@gmail.com>
2024-04-07 02:14:43 +0100

    Refactor code and add image prompts

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ const { promptGeminiPro, promptGeminiProVision } = require("./api/helper.js");
 
 /*
 
-  // example for prompting the Gemini Pro Vision model
+*    example for prompting the Gemini Pro Vision model
+*        -add images within asset directory.
+*        -define the informations related to the images.
+*        -pass your prompt and the images array as arguments to the promptGeminiProVision function.
+*/
 
+/*
   const images = [
   { name: "goku.jpg", mimeType: "image/jpeg" },
   { name: "eliot.jpg", mimeType: "image/jpeg" },
 ];
-  //calling the mehhod
-
+  
 promptGeminiProVision("What is goku doing? and wb eliot ?", images);
 */
 
 /*
-// example for prompting the Gemini Pro model
-
-
+ 
+*  example for prompting the Gemini Pro model
+*        -pass your prompt as an argument to the promptGeminiPro function.
+*/
+/*
 promptGeminiPro("how are you?");
 */
